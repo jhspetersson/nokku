9c356b19b842a0c58845e9e2131a8aaae6bc481c
xenozoid <xenozoid@gmail.com>
2019-08-12 10:30:37 +0300

    allow multiple target hosts

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
 # nokku
-Port-knocking client.
+Port-knocking client. Supports multiple hosts knocking.
 
 ### Usage
 
-    nokku HOSTNAME PORT1 [PORT2 [PORT3 [...]]
+    nokku HOSTNAME[:PORT] [HOSTNAME2:]PORT2 [[HOSTNAME3:]PORT3 [[HOSTNAME4:]PORT4 [...]]]
+    
+Supply a hostname first, optionally with a port in the form of `host:port`. 
+Then goes any number of ports. Supply another hostname option to switch the target address.
   
 ### Examples
 
+Same host, several ports:
+
     nokku 192.168.251.34 31785 23077 46254
+    nokku 192.168.251.34:31785 23077 46254
+
+Multiple hosts, several ports on each:
+
+    nokku 192.168.251.1 20100 20123 192.168.251.2 14500 14600
+    nokku 192.168.251.1:20100 20123 192.168.251.2:14500 14600
+
+Multiple hosts, one port on each:
+
+    nokku 192.168.251.1 20100 192.168.251.2 14500 192.168.251.3 33500
+    nokku 192.168.251.1:20100 192.168.251.2:14500 192.168.251.3:33500
+    
+Multiple hosts, same port:
+
+    nokku 192.168.251.1 44555 192.168.251.2 192.168.251.3
+    nokku 192.168.251.1:44555 192.168.251.2 192.168.251.3
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"regexp"
+	"strings"
 )
 
 func main() {
@@ -12,20 +16,58 @@ func main() {
 	if len(args) < 3 {
 		fmt.Println("Port-knocking client.")
 		fmt.Println("https://github.com/jhspetersson/nokku")
-		fmt.Println("Usage: nokku HOSTNAME PORT1 [PORT2 [PORT3 [...]]]")
+		fmt.Println("Usage: nokku HOSTNAME[:PORT] [HOSTNAME2:]PORT2 [[HOSTNAME3:]PORT3 [[HOSTNAME4:]PORT4 [...]]]")
 		os.Exit(1)
 	}
 
-	host := args[1]
+	var host, port string
 
-	for _, port := range args[2:] {
-		addr := host + ":" + port
-		fmt.Println("Knocking to " + addr)
-		conn, err := net.Dial("tcp", addr)
-		if err == nil {
-			_ = conn.Close()
+	for _, arg := range args[1:] {
+		parsedHost, parsedPort, err := parseArg(arg)
+
+		if err != nil {
+			log.Fatalf("Error: %s", err)
+		}
+
+		if parsedHost != "" {
+			host = parsedHost
+		}
+
+		if parsedPort != "" {
+			if host == "" {
+				log.Fatal("set hostname first")
+			}
+
+			port = parsedPort
+		}
+
+		if host != "" && port != "" {
+			addr := host + ":" + port
+
+			fmt.Println("Knocking to " + addr)
+
+			conn, err := net.Dial("tcp", addr)
+			if err == nil {
+				_ = conn.Close()
+			}
 		}
 	}
 
 	fmt.Println("Sequence completed!")
+}
+
+func parseArg(arg string) (host, port string, err error) {
+	if strings.Contains(arg, ":") {
+		parts := strings.Split(arg, ":")
+		host = parts[0]
+		port = parts[1]
+	} else if strings.Contains(arg, ".") {
+		host = arg
+	} else if ok, err := regexp.MatchString("^\\d+$", arg); ok && err == nil {
+		port = arg
+	} else {
+		err = errors.New("could not parse argument")
+	}
+
+	return
 }
